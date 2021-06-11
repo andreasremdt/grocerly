7c2265a4cfb7e09f2f039c97758c7e56734bfb3e
Andreas Remdt <me@andreasremdt.com>
2021-06-11 21:32:44 +0200

    feat: add empty state

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -11,11 +11,22 @@ type ListProps = {
 function List({ groceries, onDelete, onToggle }: ListProps) {
   return (
     <main className={styles.wrapper}>
-      <ul className={styles.list}>
-        {groceries.map((item) => (
-          <Item key={item.id} onDelete={onDelete} onToggle={onToggle} item={item} />
-        ))}
-      </ul>
+      {groceries.length ? (
+        <ul className={styles.list}>
+          {groceries.map((item) => (
+            <Item key={item.id} onDelete={onDelete} onToggle={onToggle} item={item} />
+          ))}
+        </ul>
+      ) : (
+        <div className={styles.empty}>
+          <img src="/shopping-cart.svg" width="250" alt="" />
+          <p>
+            Nothing here, yet.
+            <br />
+            Start adding items to your shopping list below.
+          </p>
+        </div>
+      )}
     </main>
   );
 }
