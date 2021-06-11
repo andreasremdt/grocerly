df890e85817996de5974b8ef0bb590f675005c29
Andreas Remdt <me@andreasremdt.com>
2021-06-11 21:41:50 +0200

    feat: persist data in `localStorage`

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useReducer } from "react";
+import { useEffect, useReducer } from "react";
 
 import Header from "./components/Header";
 import List from "./components/List";
@@ -8,12 +8,24 @@ import reducer from "./reducer";
 import { Grocery } from "./types";
 
 function App() {
-  const [groceries, dispatch] = useReducer(reducer, []);
+  const [groceries, dispatch] = useReducer(reducer, [], () => {
+    const data = localStorage.getItem("data");
+
+    if (data) {
+      return JSON.parse(data);
+    }
+
+    return [];
+  });
 
   const handleSubmit = (payload: Grocery) => dispatch({ type: "ADD_ITEM", payload });
   const handleDeleteAll = () => dispatch({ type: "DELETE_ALL" });
   const handleDelete = (payload: Grocery) => dispatch({ type: "DELETE_ITEM", payload });
   const handleToggle = (payload: Grocery) => dispatch({ type: "TOGGLE_CHECK_ITEM", payload });
+
+  useEffect(() => {
+    localStorage.setItem("data", JSON.stringify(groceries));
+  }, [groceries]);
 
   return (
     <>
