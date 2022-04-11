48ca3939c325960bbf92854698cb62d2f9477758
Andreas Remdt <me@andreasremdt.com>
2022-04-11 16:02:18 +0200

    feat: add service worker

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import "./index.css";
 
 render(
@@ -13,3 +14,5 @@ render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+serviceWorkerRegistration.register();
